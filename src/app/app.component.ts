531114b6355348479f56a4cd0c9cdc4cee90fb80
diego <diegod9405@gmail.com>
2024-10-08 22:26:22 -0600

    added menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from "./layout/layout.component";
+import { initFlowbite } from 'flowbite';
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'expenses';
+  ngOnInit(): void {
+    initFlowbite
+  }
 }
