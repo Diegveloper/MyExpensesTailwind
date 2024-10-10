cef338d29734e90c45e30d894e2f7a1a7ff41d3c
diego <diegod9405@gmail.com>
2024-10-09 21:47:35 -0600

    added charts

--- src/app/analytics/analytics.page/analytics.page.component.spec.ts
+++ src/app/analytics/analytics.page/analytics.page.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AnalyticsPageComponent } from './analytics.page.component';
 
+
+
 describe('AnalyticsPageComponent', () => {
   let component: AnalyticsPageComponent;
   let fixture: ComponentFixture<AnalyticsPageComponent>;
--- src/app/analytics/analytics.page/analytics.page.component.ts
+++ src/app/analytics/analytics.page/analytics.page.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-analytics.page',
   standalone: true,
-  imports: [],
+  imports: [ChartModule],
   templateUrl: './analytics.page.component.html',
   styleUrl: './analytics.page.component.css'
 })
-export class AnalyticsPageComponent {
+export class AnalyticsPageComponent implements OnInit{
+  data: any;
 
+    options: any;
+
+    ngOnInit() {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+        
+        this.data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+                    borderColor: documentStyle.getPropertyValue('--blue-500'),
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My Second dataset',
+                    backgroundColor: documentStyle.getPropertyValue('--pink-500'),
+                    borderColor: documentStyle.getPropertyValue('--pink-500'),
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+
+        this.options = {
+            maintainAspectRatio: false,
+            aspectRatio: 0.8,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary,
+                        font: {
+                            weight: 500
+                        }
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                }
+
+            }
+        };
+    }
 }
