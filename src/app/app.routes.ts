ff64dc3372af833d95dd0fe3d1f1f0ee672082f9
diego <diegod9405@gmail.com>
2024-10-09 21:39:00 -0600

    added analytics page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PageComponent } from './expenses/page/page.component';
+import { AnalyticsPageComponent } from './analytics/analytics.page/analytics.page.component';
 
 export const routes: Routes = [
     {path: 'expenses', component:PageComponent},
+    {path: 'analytics', component:AnalyticsPageComponent},
     {path: '**', component:PageComponent}
 ];
